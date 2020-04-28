a886f1233c2ddddea003f13d46819bc4c3a508e8
github username <github email address>
2020-04-28 15:25:26 -0600

    Complete form

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("hi")
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("%cDOM Content Loaded and Parsed!", "color: magenta")
+
+
+    const calculatorForm = document.querySelector('.calculator-form')
+    const recoveryTime = document.querySelector('.recovery-time')
+
+    calculatorForm.addEventListener('submit', () => {
+        event.preventDefault()
+        calculateRecovery(calculatorForm)
+    })
+
+    function calculateRecovery(form) {
+        const formData = new FormData(form)
+        const activityType = formData.get("activity-type")
+        const duration = formData.get("duration")
+        const intensity = formData.get("intensity")
+        const password = formData.get("password")
+
+        recoveryTime.innerText = `Recovery Time: ${(duration * intensity * activityType) / 20} Hours`
+
+    }
+
+})
